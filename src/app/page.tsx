999b98c686833b322c11d3e6457109d477d6b0e6
jecombe <jeremcombe@hotmail.fr>
2025-02-06 22:11:47 +0100

    add: navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/navBar/NavBar";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavBar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+ 
+      </div>
+    </main>
+  );
+}
